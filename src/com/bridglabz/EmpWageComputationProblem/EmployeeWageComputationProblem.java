3dea23759ae81241b817c88ca07c4f6c3f572056
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-17 18:34:22 +0530

    Added: Employe is present or absent condition

--- src/com/bridglabz/EmpWageComputationProblem/EmployeeWageComputationProblem.java
+++ src/com/bridglabz/EmpWageComputationProblem/EmployeeWageComputationProblem.java
@@ -3,5 +3,16 @@ package com.bridglabz.EmpWageComputationProblem;
 public class EmployeeWageComputationProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee computation program");
+        isEmployee();
+    }
+    public static void isEmployee(){
+        int is_Full=1;
+        double empCheck=Math.floor(Math.random()*10)%2;
+        if(empCheck==is_Full){
+
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is absent");
+        }
     }
 }
