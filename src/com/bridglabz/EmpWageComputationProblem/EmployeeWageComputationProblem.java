6747a1ca639520ace6af66c81ac28431825e365f
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-17 18:12:27 +0530

    Added: Welcome to employee management progrem

---
+++ src/com/bridglabz/EmpWageComputationProblem/EmployeeWageComputationProblem.java
@@ -0,0 +1,7 @@
+package com.bridglabz.EmpWageComputationProblem;
+
+public class EmployeeWageComputationProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee computation program");
+    }
+}
